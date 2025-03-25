35673d422538347808a04d3248a8c14e72d94439
Shezod Usmonov <sherzod.usmon.91@gmail.com>
2025-03-25 11:59:37 +0500

    added react app

--- api/app.js
+++ api/app.js
@@ -18,6 +18,10 @@ app.get('/', (req, res) => {
     {
       "id":"3",
       "title":"Show Review: Alice in Borderland"
+    },
+    {
+      "id":"3",
+      "title":"Show Review: Alice in Borderland"
     }
   ])
 })
